24514f5f7584107f68fb6b64dc286cb397f954b9
andrew <andrewmwongera>
2018-07-05 22:54:08 +0300

    This is the second commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vill Care </title>
+	<meta viewport="">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet"  href="css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="container">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="images/villacare log.png" class="logo">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="activ"><a href="index.php">HOME<span class="sr-only">(current)</span></a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">FOR SALE  </a>
+          <ul class="dropdown-menu">
+            <li ><a href="#">Flat & Apartments</a></li>
+             <li role="separator" class="divider"></li>
+            <li ><a href="#">Houses</a></li>
+             <li role="separator" class="divider"></li>
+            <li ><a href="#">Commercial Properties</a></li>
+            	
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">FOR RENT  </a>
+          <ul class="dropdown-menu">
+            <li ><a href="#">Flat & Apartments</a></li>
+             <li role="separator" class="divider"></li>
+            <li ><a href="#">Houses</a></li>
+             <li role="separator" class="divider"></li>
+            <li ><a href="#">Commercial Properties</a></li>
+      		<li role="separator" class="divider"></li>
+            <li ><a href="#">Bedsitter</a></li>
+            	
+          </ul>
+        </li>
+        <li class="activ"><a href="services.php">SERVICES<span class="sr-only">(current)</span></a></li>
+        <li class="activ"><a href="contact.php">CONTACT<span class="sr-only">(current)</span></a></li>
+      </ul>
+      <div>
+      	<button class="btn btn-primary list">List Your Propery</button>
+      	
+
+      </div>
+      
+     
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav><!--- ====== end of nav ====== -->
+
+	<!--- ====== start  of Landing Area ====== -->
+	
+		<div class="landarea" id="bg">
+			<div class="quote">
+			<h1>Buy, sell and  rent property online</h1>
+		</div>
+		<div id="formsearch">
+			<form class="navbar-form " role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="enter for rental,sales,buying">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+		</div>
+
+
+	</div>
+		<!--- ====== end of Landing Area ====== -->
+</div>
+
+
+<script src="js/jquery-1.11.1.min.js"></script>
+<script  src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
